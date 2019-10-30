fe1f6d339d0b704f5e1925ef5ea676eff97c5dd7
SeverinHersche <35527435+SeverinHersche@users.noreply.github.com>
2019-10-30 12:52:10 +0100

    Add files via upload

---
+++ Demo/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+
+namespace Demo
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MercuryZelle.Blocks.Add(new Paragraph(new Run("Mercury")));
+            var tabel = new TableCell();
+            tabel.Blocks.Add(new Paragraph(new Run("Vom Code erstellt")));
+            TableRowTest.Cells.Add(tabel);
+        }
+    }
+}
